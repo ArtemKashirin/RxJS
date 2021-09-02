3d23c0bf33993e4c53bcaefe75a9eca0e2fc154a
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-02 22:45:42 +0300

    Operators: interval

--- src/app/functions/generative/interval/interval.component.ts
+++ src/app/functions/generative/interval/interval.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval} from 'rxjs';
 
 @Component({
   selector: 'app-interval',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interval.component.scss']
 })
 export class IntervalComponent implements OnInit {
+  public intervalLink: string = 'https://rxjs.dev/api/index/function/interval';
+  public interval: string = 'interval(period: number = 0, scheduler: SchedulerLike = asyncScheduler): Observable<number>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  interva() {
+    interval(1000).subscribe(value => console.log('interval:', value))
+  }
 }
