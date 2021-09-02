fbb1af314c0c4f4ac6ec7508d32698e37d1fb87d
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-02 23:08:24 +0300

    Operators: range

--- src/app/functions/generative/range/range.component.ts
+++ src/app/functions/generative/range/range.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {range} from "rxjs";
 
 @Component({
   selector: 'app-range',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./range.component.scss']
 })
 export class RangeComponent implements OnInit {
+  public rangeLink: string = 'https://rxjs.dev/api/index/function/range';
+  public range: string = 'range(start: number, count?: number): Observable<number>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  rang() {
+    range(0, 10).subscribe(value => console.log('range:', value));
+  }
 }
