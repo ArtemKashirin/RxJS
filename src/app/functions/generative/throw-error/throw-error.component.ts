d59dc86f15297f159149bd8f8a7e5c28861e2bfa
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-02 23:34:39 +0300

    Operators: throwError

--- src/app/functions/generative/throw-error/throw-error.component.ts
+++ src/app/functions/generative/throw-error/throw-error.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable, of, throwError} from 'rxjs';
+import {interval} from "rxjs";
+import {concatMap, map} from "rxjs/operators";
 
 @Component({
   selector: 'app-throw-error',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./throw-error.component.scss']
 })
 export class ThrowErrorComponent implements OnInit {
+  public throwErrorLink: string = 'https://rxjs.dev/api/index/function/throwError';
+  public throwError: string = 'throwError(errorOrErrorFactory: any): Observable<never>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  throwErro() {
+    throwError('Error').subscribe(null, error => console.log(' throwError:', error))
+
+  }
 }
