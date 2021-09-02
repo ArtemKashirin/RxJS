d603c390bdf4b91f11f4950141f894cbced68dfb
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-02 22:32:48 +0300

    Operators: fromEvent

--- src/app/functions/generative/from-event/from-event.component.ts
+++ src/app/functions/generative/from-event/from-event.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fromEvent} from "rxjs";
 
 @Component({
   selector: 'app-from-event',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./from-event.component.scss']
 })
 export class FromEventComponent implements OnInit {
+  public fromEventLink: string = 'https://rxjs.dev/api/index/function/fromEvent';
+  public fromEvent: string = 'fromEvent<T>(target: any, eventName: string, options?: EventListenerOptions | ((...args: any[]) => T), resultSelector?: (...args: any[]) => T): Observable<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  fromEven() {
+    fromEvent(document, 'click').subscribe(value => {
+      (value.target as HTMLElement).style.backgroundColor = 'blue';
+      console.log('fromEvent:', value)})
+  }
 }
