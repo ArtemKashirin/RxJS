4ac3ce2b369954eecf3e7977c57e2ba7239cebd8
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-02 22:19:15 +0300

    Operators: defer

--- src/app/functions/generative/defer/defer.component.ts
+++ src/app/functions/generative/defer/defer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {defer, from, interval, Observable} from "rxjs";
 
 @Component({
   selector: 'app-defer',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./defer.component.scss']
 })
 export class DeferComponent implements OnInit {
+  public deferLink: string = 'https://rxjs.dev/api/index/function/defer';
+  public defer: string = 'defer<R extends ObservableInput<any>>(observableFactory: () => R): Observable<ObservedValueOf<R>>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  defe() {
+    const isTrue: boolean = true;
+    const o: Observable<string> = from(['qw', 'er', 'ty']);
+    const i: Observable<number> = interval(1000);
+
+    defer<Observable<string | number>>(() => {
+      if(isTrue) return o
+      else return i
+    }).subscribe(value => console.log('defer:', value))
+  }
 }
