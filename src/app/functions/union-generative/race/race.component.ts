4a0b372ad8993b3f80e7f034676704333e80ee62
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-03 17:29:27 +0300

    Operators: race

--- src/app/functions/union-generative/race/race.component.ts
+++ src/app/functions/union-generative/race/race.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {race} from 'rxjs';
+import {interval, Observable} from "rxjs";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'app-race',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./race.component.scss']
 })
 export class RaceComponent implements OnInit {
+  public raceLink: string = 'https://rxjs.dev/api/index/function/race';
+  public race: string = 'race<T>(...sources: (Observable<T> | InteropObservable<T> | AsyncIterable<T> | PromiseLike<T> | ArrayLike<T> | Iterable<...> | ReadableStreamLike<...> | ObservableInput<...>[])[]): Observable<any>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  rac() {
+    const firstObs: Observable<number> = interval(2000).pipe(take(4));
+    const secondObs: Observable<number> = interval(1000).pipe(take(2));
+    race(firstObs, secondObs).subscribe(value => console.log('race:', value));
+  }
 }
