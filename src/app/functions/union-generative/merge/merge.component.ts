82e003dce786ba04f1b38e60207db9411476c047
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-03 17:05:20 +0300

    Operators: merge

--- src/app/functions/union-generative/merge/merge.component.ts
+++ src/app/functions/union-generative/merge/merge.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { merge } from 'rxjs';
+import {interval, Observable} from "rxjs";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'app-merge',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./merge.component.scss']
 })
 export class MergeComponent implements OnInit {
+  public mergeLink: string = 'https://rxjs.dev/api/index/function/merge';
+  public merge: string = 'merge(...args: (number | SchedulerLike | Observable<unknown> | InteropObservable<unknown> | AsyncIterable<unknown> | PromiseLike<unknown> | ArrayLike<...> | Iterable<...> | ReadableStreamLike<...>)[]): Observable<unknown>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  merg() {
+    const firstObs: Observable<number> = interval(2000).pipe(take(4));
+    const secondObs: Observable<number> = interval(1000).pipe(take(2));
+    merge(firstObs, secondObs).subscribe(value => console.log('merge:', value));
+  }
 }
