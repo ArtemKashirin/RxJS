c1757e9c2773261c5d97006a3549f589fc4b958c
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-03 14:29:22 +0300

    Operators: generate

--- src/app/functions/generative/generate/generate.component.ts
+++ src/app/functions/generative/generate/generate.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {generate} from "rxjs";
 
 @Component({
   selector: 'app-generate',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./generate.component.scss']
 })
 export class GenerateComponent implements OnInit {
+  public generateLink: string = 'https://rxjs.dev/api/index/function/generate';
+  public generate: string = 'generate(options: GenerateBaseOptions<S>): Observable<S>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  generat() {
+    generate({
+      initialState: 0,
+      condition: x => x < 3,
+      iterate: x => x + 1,
+    }).subscribe(value => console.log('generate:', value))
+
+  }
 }
