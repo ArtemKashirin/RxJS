a224a3fc32a27807623a96584090ac11e6099880
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-03 15:40:49 +0300

    Operators: concat

--- src/app/functions/union-generative/concat/concat.component.ts
+++ src/app/functions/union-generative/concat/concat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {concat, interval, Observable} from "rxjs";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'app-concat',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./concat.component.scss']
 })
 export class ConcatComponent implements OnInit {
+  public concatLink: string = 'https://rxjs.dev/api/index/function/concat';
+  public concat: string = 'concat(...args: any[]): Observable<unknown>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  conca() {
+    const firstObs: Observable<number> = interval(2000).pipe(take(4));
+    const secondObs: Observable<number> = interval(1000).pipe(take(2));
+    concat(firstObs, secondObs).subscribe(value => console.log('concat:', value));
+  }
 }
