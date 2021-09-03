6b21960cbe85a5a158498bf0d3e8ec9960deb8b0
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-03 15:23:22 +0300

    Operators: forkJoin

--- src/app/functions/union-generative/fork-join/fork-join.component.ts
+++ src/app/functions/union-generative/fork-join/fork-join.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import {interval, Observable} from "rxjs";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'app-fork-join',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fork-join.component.scss']
 })
 export class ForkJoinComponent implements OnInit {
+  public forkJoinLink: string = 'https://rxjs.dev/api/index/function/forkJoin';
+  public forkJoin: string = 'forkJoin(...args: any[]): Observable<any>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  forkJoi() {
+    const firstObs: Observable<number> = interval(1100).pipe(take(4));
+    const secondObs: Observable<number> = interval(1000).pipe(take(2));
+    forkJoin([firstObs, secondObs]).subscribe(value => console.log('forkJoin:', value));
+  }
 }
