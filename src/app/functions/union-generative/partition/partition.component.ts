afe6c64979d04584a8e0159f4fde15db6f8ac40d
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-03 17:21:50 +0300

    Operators: partition

--- src/app/functions/union-generative/partition/partition.component.ts
+++ src/app/functions/union-generative/partition/partition.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {partition} from 'rxjs';
+import {interval, Observable} from "rxjs";
+import {take} from "rxjs/operators";
 
 @Component({
   selector: 'app-partition',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./partition.component.scss']
 })
 export class PartitionComponent implements OnInit {
+  public partitionLink: string = 'https://rxjs.dev/api/index/function/partition';
+  public partition: string = 'partition<T>(source: ObservableInput<T>, predicate: (this: any, value: T, index: number) => boolean, thisArg?: any): [Observable<T>, Observable<T>]';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  partitio() {
+    const [first$, second$] = partition(interval(500).pipe(take(10)), value => value > 5);
+    first$.subscribe(value => console.log('partition:', value))
+
+  }
 }
