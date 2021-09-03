d5b72144796aec661365a92bdbf1543367c621ee
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-03 22:27:41 +0300

    Operators: sample

---
+++ src/app/functions/operators/sample/sample.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {interval} from "rxjs";
+import {sample} from "rxjs/operators";
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './sample.component.html',
+  styleUrls: ['./sample.component.scss']
+})
+export class SampleComponent implements OnInit {
+  public sampleLink: string = 'https://rxjs.dev/api/operators/sample';
+  public sample: string = 'sample<T>(notifier: Observable<any>): MonoTypeOperatorFunction<T>';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  sampl() {
+    interval(500)
+      .pipe(sample(interval(1000)))
+      .subscribe(value => console.log('sample:', value))
+
+  }
+}
