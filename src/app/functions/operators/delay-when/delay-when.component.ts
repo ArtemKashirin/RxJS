9e2658916e1925afd1860ce654a97f4af109320b
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-04 19:42:04 +0300

    Operators: delayWhen

--- src/app/functions/operators/delay-when/delay-when.component.ts
+++ src/app/functions/operators/delay-when/delay-when.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval, of} from "rxjs";
+import {delayWhen} from "rxjs/operators";
 
 @Component({
   selector: 'app-delay-when',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delay-when.component.scss']
 })
 export class DelayWhenComponent implements OnInit {
+  public delayWhenLink: string = 'https://rxjs.dev/api/operators/delayWhen';
+  public delayWhen: string = 'delayWhen<T>(delayDurationSelector: (value: T, index: number) => Observable<any>, subscriptionDelay?: Observable<any>): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  delayWhe() {
+    interval(1000)
+    of(1, 2, 3, 4, 5)
+      .pipe(delayWhen(() => interval(5000)))
+      .subscribe(value => console.log('delayWhen:', value))
+
+  }
 }
