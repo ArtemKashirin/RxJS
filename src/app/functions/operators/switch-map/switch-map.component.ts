133dee01fa81ebbd3748a5cbc421a8bc72cd6e9d
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-04 18:14:41 +0300

    Operators: switchMap

--- src/app/functions/operators/switch-map/switch-map.component.ts
+++ src/app/functions/operators/switch-map/switch-map.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval, of} from "rxjs";
+import {switchMap, take} from "rxjs/operators";
 
 @Component({
   selector: 'app-switch-map',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./switch-map.component.scss']
 })
 export class SwitchMapComponent implements OnInit {
+  public switchMapLink: string = 'https://rxjs.dev/api/operators/switchMap';
+  public switchMap: string = 'switchMap<T, R, O extends ObservableInput<any>>(project: (value: T, index: number) => O, resultSelector?: (outerValue: T, innerValue: ObservedValueOf<O>, outerIndex: number, innerIndex: number) => R): OperatorFunction<T, ObservedValueOf<O> | R>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  switchMa() {
+    interval(1000)
+      .pipe(
+        take(5),
+        switchMap(i => of(i + 10)))
+      .subscribe(value => console.log('exhaustMap:', value))
+  }
 }
