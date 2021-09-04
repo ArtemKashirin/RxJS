34dcaa5c7784b9410c4a8b57db46ff38cc532da1
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-04 23:05:12 +0300

    Operators: distinctUntilChanged

--- src/app/functions/operators/distinct-until-changed/distinct-until-changed.component.ts
+++ src/app/functions/operators/distinct-until-changed/distinct-until-changed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {from} from "rxjs";
+import {distinctUntilChanged} from "rxjs/operators";
 
 @Component({
   selector: 'app-distinct-until-changed',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./distinct-until-changed.component.scss']
 })
 export class DistinctUntilChangedComponent implements OnInit {
+  public distinctUntilChangedLink: string = 'https://rxjs.dev/api/operators/distinctUntilChanged';
+  public distinctUntilChanged: string = 'distinctUntilChanged<T, K>(comparator?: (previous: K, current: K) => boolean, keySelector: (value: T) => K = identity as (value: T) => K): MonoTypeOperatorFunction<T>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  distinctUntilChange() {
+    from([1, 1, 2, 2, 3, 3, 4, 4, 5, 5])
+      .pipe(distinctUntilChanged())
+      .subscribe(value => console.log('distinctUntilChanged:', value))
+  }
 }
