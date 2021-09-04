2ca33fc0bef190fb4de860ab7e3cfd9eeded72f1
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-04 17:25:24 +0300

    Operators: takeUntil

--- src/app/functions/operators/take-until/take-until.component.ts
+++ src/app/functions/operators/take-until/take-until.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fromEvent, interval} from "rxjs";
+import {take, takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-take-until',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./take-until.component.scss']
 })
 export class TakeUntilComponent implements OnInit {
+  public takeUntilLink: string = 'https://rxjs.dev/api/operators/takeUntil';
+  public takeUntil: string = 'takeUntil<T>(notifier: ObservableInput<any>): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+
+  }
+
+  takeUnti() {
+    interval(1000).pipe(takeUntil(interval(5000)))
+      .subscribe(value => console.log('takeUntil:', value))
+
+  }
 }
