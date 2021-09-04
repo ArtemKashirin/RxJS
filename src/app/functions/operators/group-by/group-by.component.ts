acbc5465b406f8f13df5ddae0ac51c1a59d90c14
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-04 22:53:02 +0300

    Operators: groupBy

--- src/app/functions/operators/group-by/group-by.component.ts
+++ src/app/functions/operators/group-by/group-by.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from "rxjs";
+import {groupBy} from "rxjs/operators";
 
 @Component({
   selector: 'app-group-by',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-by.component.scss']
 })
 export class GroupByComponent implements OnInit {
+  public groupByLink: string = 'https://rxjs.dev/api/operators/groupBy';
+  public groupBy: string = 'groupBy<T, K, R>(keySelector: (value: T) => K, elementOrOptions?: void | ((value: any) => any) | BasicGroupByOptions<K, T> | GroupByOptionsWithElement<K, R, T>, duration?: (grouped: GroupedObservable<any, any>) => ObservableInput<any>, connector?: () => SubjectLike<any>): OperatorFunction<T, GroupedObservable<K, R>>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  groupB() {
+    of(
+      {id: 1, name: 'JavaScript'},
+      {id: 2, name: 'Parcel'},
+      {id: 2, name: 'webpack'},
+      {id: 1, name: 'TypeScript'},
+      {id: 7, name: 'TSLint'}
+    ).pipe(groupBy(p => p.id)).subscribe(value => console.log('groupBy:', value))
+  }
 }
