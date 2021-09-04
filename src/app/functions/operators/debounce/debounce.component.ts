ec6db334c60e4e7bbe90e139f99db9a1f0fa98f3
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-04 19:20:49 +0300

    Operators: debounce

--- src/app/functions/operators/debounce/debounce.component.ts
+++ src/app/functions/operators/debounce/debounce.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fromEvent, timer} from 'rxjs';
+import {interval, of} from "rxjs";
+import {debounce, delay} from "rxjs/operators";
 
 @Component({
   selector: 'app-debounce',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./debounce.component.scss']
 })
 export class DebounceComponent implements OnInit {
+  public debounceLink: string = 'https://rxjs.dev/api/operators/debounce';
+  public debounce: string = 'debounce<T>(durationSelector: (value: T) => ObservableInput<any>): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  debounc() {
+    of(1, 2, 3).pipe(
+      delay(5000),
+      debounce(() => interval(500)))
+      .subscribe(value => console.log('debounce:', value))
+  }
 }
