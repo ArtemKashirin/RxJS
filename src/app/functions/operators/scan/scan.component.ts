f3f20341ba25b85c837c5c9a16602ea1d2a260f0
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-04 22:04:20 +0300

    Operators: scan

--- src/app/functions/operators/scan/scan.component.ts
+++ src/app/functions/operators/scan/scan.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from "rxjs";
+import {scan} from "rxjs/operators";
 
 @Component({
   selector: 'app-scan',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scan.component.scss']
 })
 export class ScanComponent implements OnInit {
+  public scanLink: string = 'https://rxjs.dev/api/operators/scan';
+  public scan: string = 'scan<V, A, S>(accumulator: (acc: V | A | S, value: V, index: number) => A, seed?: S): OperatorFunction<V, V | A>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  sca() {
+    of(1, 2, 3, 4, 5)
+      .pipe(scan((acc, value) => acc + value))
+      .subscribe(value => console.log('scan:', value))
+
+  }
 }
