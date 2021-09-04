b0963ff87a2ce6811ef7ab84dd37f52cc5028356
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-04 18:30:14 +0300

    Operators: mergeScan

--- src/app/functions/operators/merge-scan/merge-scan.component.ts
+++ src/app/functions/operators/merge-scan/merge-scan.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from "rxjs";
+import {mergeScan} from "rxjs/operators";
 
 @Component({
   selector: 'app-merge-scan',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./merge-scan.component.scss']
 })
 export class MergeScanComponent implements OnInit {
+  public mergeScanLink: string = 'https://rxjs.dev/api/operators/mergeScan';
+  public mergeScan: string = 'mergeScan<T, R>(accumulator: (acc: R, value: T, index: number) => ObservableInput<R>, seed: R, concurrent: number = Infinity): OperatorFunction<T, R>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  mergeSca() {
+    of(1, 2, 3, 4)
+      .pipe(mergeScan((acc, value) => of(acc + value), 0))
+      .subscribe(value => console.log('mergeScan:', value))
+
+  }
 }
