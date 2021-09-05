5347c7e8353dba06413ec37bb01be416dcd7f38d
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 15:57:10 +0300

    Operators: reduce

--- src/app/functions/operators/reduce/reduce.component.ts
+++ src/app/functions/operators/reduce/reduce.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from "rxjs";
+import {reduce} from "rxjs/operators";
 
 @Component({
   selector: 'app-reduce',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reduce.component.scss']
 })
 export class ReduceComponent implements OnInit {
+  public reduceLink: string = 'https://rxjs.dev/api/operators/reduce';
+  public reduce: string = 'reduce<V, A>(accumulator: (acc: V | A, value: V, index: number) => A, seed?: any): OperatorFunction<V, V | A>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  reduc() {
+    of(1, 2, 3, 4, 5)
+      .pipe(reduce((acc, value) => acc + value))
+      .subscribe(value => console.log('reduce:', value))
+  }
 }
