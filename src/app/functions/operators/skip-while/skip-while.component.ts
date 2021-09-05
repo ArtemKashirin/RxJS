a2be48425384e344b1c907f960cfe650a08fd923
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 14:46:37 +0300

    Operators: skipWhile

--- src/app/functions/operators/skip-while/skip-while.component.ts
+++ src/app/functions/operators/skip-while/skip-while.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval} from "rxjs";
+import {skipWhile} from "rxjs/operators";
 
 @Component({
   selector: 'app-skip-while',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skip-while.component.scss']
 })
 export class SkipWhileComponent implements OnInit {
+  public skipWhileLink: string = 'https://rxjs.dev/api/operators/skipWhile';
+  public skipWhile: string = 'skipWhile<T>(predicate: (value: T, index: number) => boolean): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  skipWhil() {
+    interval(1000)
+      .pipe(skipWhile(value => value < 4))
+      .subscribe(value => console.log('skipWhile:', value))
+  }
 }
