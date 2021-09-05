423ea1936cb4c4498ddc9fbb5e3fd498716dc332
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 14:58:10 +0300

    Operators: findIndex

--- src/app/functions/operators/find-index/find-index.component.ts
+++ src/app/functions/operators/find-index/find-index.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {of} from "rxjs";
+import {findIndex} from "rxjs/operators";
 
 @Component({
   selector: 'app-find-index',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find-index.component.scss']
 })
 export class FindIndexComponent implements OnInit {
+  public findIndexLink: string = 'https://rxjs.dev/api/operators/findIndex';
+  public findIndex: string = 'findIndex<T>(predicate: (value: T, index: number, source: Observable<T>) => boolean, thisArg?: any): OperatorFunction<T, number>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  findInde() {
+    of(1, 2, 3, 4)
+      .pipe(findIndex(value => value < 5))
+      .subscribe(value => console.log('findIndex:', value))
+  }
 }
