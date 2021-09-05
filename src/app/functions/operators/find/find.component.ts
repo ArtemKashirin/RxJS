bc798e463445ebe5f8f8f2d0af4f4280c474eb87
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 14:55:26 +0300

    Operators: find

--- src/app/functions/operators/find/find.component.ts
+++ src/app/functions/operators/find/find.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {of} from "rxjs";
+import {find} from "rxjs/operators";
 
 @Component({
   selector: 'app-find',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find.component.scss']
 })
 export class FindComponent implements OnInit {
+  public findLink: string = 'https://rxjs.dev/api/operators/find';
+  public find: string = 'find<T>(predicate: (value: T, index: number, source: Observable<T>) => boolean, thisArg?: any): OperatorFunction<T, T | undefined>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  fin() {
+    of(1, 2, 3, 4)
+      .pipe(find(value => value < 5))
+      .subscribe(value => console.log('find:', value))
+  }
 }
