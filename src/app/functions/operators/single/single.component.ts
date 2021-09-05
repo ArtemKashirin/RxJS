df20f52e64aaa7932a1bcfd240a4cbc5b4131c87
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 14:12:00 +0300

    Operators: single

--- src/app/functions/operators/single/single.component.ts
+++ src/app/functions/operators/single/single.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {from} from "rxjs";
+import {single} from "rxjs/operators";
 
 @Component({
   selector: 'app-single',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single.component.scss']
 })
 export class SingleComponent implements OnInit {
+  public singleLink: string = 'https://rxjs.dev/api/operators/single';
+  public single: string = 'single<T>(predicate?: (value: T, index: number, source: Observable<T>) => boolean): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  singl() {
+    from([1, 1, 4, 5, 3, 7, 8])
+      .pipe(single(value => value == 3))
+      .subscribe(value => console.log('single:', value))
+  }
 }
