bc6c1b0f0cddcfeac9ce87660884e98fa5f804c8
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 16:18:26 +0300

    Operators: timestamp

--- src/app/functions/operators/timestamp/timestamp.component.ts
+++ src/app/functions/operators/timestamp/timestamp.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {interval} from "rxjs";
+import {timestamp} from "rxjs/operators";
 
 @Component({
   selector: 'app-timestamp',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timestamp.component.scss']
 })
 export class TimestampComponent implements OnInit {
+  public timestampLink: string = 'https://rxjs.dev/api/operators/timestamp';
+  public timestamp: string = 'timestamp<T>(timestampProvider: TimestampProvider = dateTimestampProvider): OperatorFunction<T, Timestamp<T>>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  timestam() {
+    interval(1000)
+      .pipe(timestamp())
+      .subscribe(value => console.log('timestamp:', value))
+  }
 }
