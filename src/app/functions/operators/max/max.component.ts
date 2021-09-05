458d3000d1f60f6a5a55ce4701be4b92037fcc16
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 15:06:54 +0300

    Operators: max

--- src/app/functions/operators/max/max.component.ts
+++ src/app/functions/operators/max/max.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {of} from "rxjs";
+import {max} from "rxjs/operators";
 
 @Component({
   selector: 'app-max',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./max.component.scss']
 })
 export class MaxComponent implements OnInit {
+  public maxLink: string = 'https://rxjs.dev/api/operators/max';
+  public max: string = 'max<T>(comparer?: (x: T, y: T) => number): MonoTypeOperatorFunction<T>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ma() {
+    of(1,4,8,44,7)
+      .pipe(max())
+      .subscribe(value => console.log('max:', value))
+
+  }
 }
