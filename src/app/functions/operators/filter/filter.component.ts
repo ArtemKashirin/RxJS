edbe77e9973ef665bdbda5df824629f494203f79
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 13:58:58 +0300

    Operators: filter

--- src/app/functions/operators/filter/filter.component.ts
+++ src/app/functions/operators/filter/filter.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {from} from "rxjs";
+import {filter} from "rxjs/operators";
 
 @Component({
   selector: 'app-filter',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
+  public filterLink: string = 'https://rxjs.dev/api/operators/filter';
+  public filter: string = 'filter<T>(predicate: (value: T, index: number) => boolean, thisArg?: any): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  filte() {
+    from([1, 1, 4, 5, 3, 7, 8])
+      .pipe(filter(value => value > 3))
+      .subscribe(value => console.log('filter:', value))
+
+  }
 }
