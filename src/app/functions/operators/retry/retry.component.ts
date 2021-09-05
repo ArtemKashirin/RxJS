08704d05e8869194d382932ca96e79e046468af7
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 18:47:23 +0300

    Operators: retry

--- src/app/functions/operators/retry/retry.component.ts
+++ src/app/functions/operators/retry/retry.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval} from "rxjs";
+import {retry, tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-retry',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./retry.component.scss']
 })
 export class RetryComponent implements OnInit {
+  public retryLink: string = 'https://rxjs.dev/api/operators/retry';
+  public retry: string = 'retry<T>(configOrCount: number | RetryConfig = Infinity): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  retr() {
+    interval(100)
+      .pipe(tap(v => {
+        if(v == 5) throw new Error()
+      }), retry(2))
+      .subscribe(value => console.log('retry:', value))
+  }
 }
