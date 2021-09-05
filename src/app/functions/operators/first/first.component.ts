a4a524cce281d696913bf045df5960446c302e63
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 14:02:40 +0300

    Operators: first

--- src/app/functions/operators/first/first.component.ts
+++ src/app/functions/operators/first/first.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {from} from "rxjs";
+import {first} from "rxjs/operators";
 
 @Component({
   selector: 'app-first',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first.component.scss']
 })
 export class FirstComponent implements OnInit {
+  public firstLink: string = 'https://rxjs.dev/api/operators/first';
+  public first: string = 'first<T, D>(predicate?: (value: T, index: number, source: Observable<T>) => boolean, defaultValue?: D): OperatorFunction<T, T | D>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  firs() {
+    from([1, 1, 4, 5, 3, 7, 8])
+      .pipe(first(value => value > 3))
+      .subscribe(value => console.log('first:', value))
+  }
 }
