ab388ebdaeb104db8ddb2faacd023d7d4c58d7d7
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 18:44:12 +0300

    Operators: retryWhen

--- src/app/functions/operators/retry-when/retry-when.component.ts
+++ src/app/functions/operators/retry-when/retry-when.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval, of} from "rxjs";
+import {retryWhen, tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-retry-when',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./retry-when.component.scss']
 })
 export class RetryWhenComponent implements OnInit {
+  public retryWhenLink: string = 'https://rxjs.dev/api/operators/retryWhen';
+  public retryWhen: string = 'retryWhen<T>(notifier: (errors: Observable<any>) => Observable<any>): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  retryWhe() {
+    interval(100)
+      .pipe(
+        tap(v => {
+          if(v == 5) throw v
+        }),
+        retryWhen((err) => err
+          .pipe(tap(v => console.log(v)))))
+      .subscribe(value => console.log('retryWhen:', value))
+  }
 }
