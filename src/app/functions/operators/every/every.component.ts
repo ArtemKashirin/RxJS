c86bff5a60848b63d38bdb0632e299516a66b12d
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 14:52:08 +0300

    Operators: every

--- src/app/functions/operators/every/every.component.ts
+++ src/app/functions/operators/every/every.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from "rxjs";
+import {every} from "rxjs/operators";
 
 @Component({
   selector: 'app-every',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./every.component.scss']
 })
 export class EveryComponent implements OnInit {
+  public everyLink: string = 'https://rxjs.dev/api/operators/every';
+  public every: string = 'every<T>(predicate: (value: T, index: number, source: Observable<T>) => boolean, thisArg?: any): OperatorFunction<T, boolean>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  ever() {
+    of(1, 2, 3, 4)
+      .pipe(every(value => value < 5))
+      .subscribe(value => console.log('every:', value))
+
+  }
 }
