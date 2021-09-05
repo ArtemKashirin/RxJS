3388688d2ffc8bdb9d013163e8fe32297f2ab3af
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 14:43:34 +0300

    Operators: takeWhile

--- src/app/functions/operators/take-while/take-while.component.ts
+++ src/app/functions/operators/take-while/take-while.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {interval} from "rxjs";
+import {takeWhile} from "rxjs/operators";
 
 @Component({
   selector: 'app-take-while',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./take-while.component.scss']
 })
 export class TakeWhileComponent implements OnInit {
+  public takeWhileLink: string = 'https://rxjs.dev/api/operators/takeWhile';
+  public takeWhile: string = 'takeWhile<T>(predicate: (value: T, index: number) => boolean, inclusive: boolean = false): MonoTypeOperatorFunction<T>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  takeWhil() {
+    interval(1000)
+      .pipe(takeWhile(value => value < 4))
+      .subscribe(value => console.log('takeWhile:', value))
+  }
 }
