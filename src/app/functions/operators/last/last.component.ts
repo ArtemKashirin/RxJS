fc2e50c213e7004ea6f83909e638a006e89dbffd
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 14:06:36 +0300

    Operators: last

--- src/app/functions/operators/last/last.component.ts
+++ src/app/functions/operators/last/last.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {from} from "rxjs";
+import {last} from "rxjs/operators";
 
 @Component({
   selector: 'app-last',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./last.component.scss']
 })
 export class LastComponent implements OnInit {
+  public lastLink: string = 'https://rxjs.dev/api/operators/last';
+  public last: string = 'last<T, D>(predicate?: (value: T, index: number, source: Observable<T>) => boolean, defaultValue?: D): OperatorFunction<T, T | D>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  las() {
+    from([1, 1, 4, 5, 3, 7, 8])
+      .pipe(last(value => value > 3))
+      .subscribe(value => console.log('last:', value))
+  }
 }
