48e996ee594d65c1e3656338565630ec4c164b25
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-05 15:02:40 +0300

    Operators: count

--- src/app/functions/operators/count/count.component.ts
+++ src/app/functions/operators/count/count.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from "rxjs";
+import {count} from "rxjs/operators";
 
 @Component({
   selector: 'app-count',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./count.component.scss']
 })
 export class CountComponent implements OnInit {
+  public countLink: string = 'https://rxjs.dev/api/operators/count';
+  public count: string = 'count<T>(predicate?: (value: T, index: number) => boolean): OperatorFunction<T, number>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  coun() {
+    of(1, 2, 3, 4, 5, 6, 7)
+      .pipe(count())
+      .subscribe(value => console.log('count:', value))
+  }
 }
