598d31796aa37ad475c6ad2b3f8ebe90abf2f6f0
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 16:36:17 +0300

    Operators: elementAt

--- src/app/functions/operators/element-at/element-at.component.ts
+++ src/app/functions/operators/element-at/element-at.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval} from "rxjs";
+import {elementAt} from "rxjs/operators";
 
 @Component({
   selector: 'app-element-at',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./element-at.component.scss']
 })
 export class ElementAtComponent implements OnInit {
+  public elementAtLink: string = 'https://rxjs.dev/api/operators/elementAt';
+  public elementAt: string = 'elementAt<T, D = T>(index: number, defaultValue?: D): OperatorFunction<T, T | D>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  elementA() {
+    interval(500)
+      .pipe(elementAt(10))
+      .subscribe(value => console.log('elementAt:', value))
+
+  }
 }
