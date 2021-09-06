fac0d9c397b1511485f830b1baacdbc932277ec9
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 14:55:10 +0300

    Operators: bufferWhen

--- src/app/functions/operators/buffer-when/buffer-when.component.ts
+++ src/app/functions/operators/buffer-when/buffer-when.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fromEvent, interval, of} from "rxjs";
+import {buffer, bufferWhen} from "rxjs/operators";
 
 @Component({
   selector: 'app-buffer-when',
@@ -6,12 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buffer-when.component.scss']
 })
 export class BufferWhenComponent implements OnInit {
-  // public Link: string = '';
-  // public Link: string = '';
+  public bufferWhenLink: string = 'https://rxjs.dev/api/operators/bufferWhen';
+  public bufferWhen: string = 'bufferWhen<T>(closingSelector: () => ObservableInput<any>): OperatorFunction<T, T[]>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  bufferWhe() {
+    fromEvent(document, 'click').pipe(
+      bufferWhen(() => interval(5000)))
+      .subscribe(value => console.log('bufferWhen:', value))
+  }
 }
