2d831d99651e89724cd0927b72783798a378a1e6
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 15:47:16 +0300

    Operators: windowCount

--- src/app/functions/operators/window-count/window-count.component.ts
+++ src/app/functions/operators/window-count/window-count.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {interval} from "rxjs";
+import {windowCount} from "rxjs/operators";
 
 @Component({
   selector: 'app-window-count',
@@ -6,12 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./window-count.component.scss']
 })
 export class WindowCountComponent implements OnInit {
-  // public Link: string = '';
-  // public Link: string = '';
+  public windowCountLink: string = 'https://rxjs.dev/api/operators/windowCount';
+  public windowCount: string = 'windowCount<T>(windowSize: number, startWindowEvery: number = 0): OperatorFunction<T, Observable<T>>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  windowCoun() {
+    interval(1000)
+      .pipe(windowCount(5))
+      .subscribe(value => console.log(value.subscribe(value1 => console.log('windowCount:', value1))))
+  }
 }
