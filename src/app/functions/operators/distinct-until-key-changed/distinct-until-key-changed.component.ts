cdd752ae23fe7d86f2b7b032f21f596b1141e55a
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 16:56:44 +0300

    Operators: distinctUntilKeyChanged

--- src/app/functions/operators/distinct-until-key-changed/distinct-until-key-changed.component.ts
+++ src/app/functions/operators/distinct-until-key-changed/distinct-until-key-changed.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from "rxjs";
+import {distinctUntilKeyChanged} from "rxjs/operators";
 
 @Component({
   selector: 'app-distinct-until-key-changed',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./distinct-until-key-changed.component.scss']
 })
 export class DistinctUntilKeyChangedComponent implements OnInit {
+  public distinctUntilKeyChangedLink: string = 'https://rxjs.dev/api/operators/distinctUntilKeyChanged';
+  public distinctUntilKeyChanged: string = 'distinctUntilKeyChanged<T, K extends keyof T>(key: K, compare?: (x: T[K], y: T[K]) => boolean): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  distinctUntilKeyChange() {
+    of(    { age: 4, name: 'Foo'},
+      { age: 7, name: 'Bar'},
+      { age: 5, name: 'Foo'},
+      { age: 6, name: 'Foo'})
+      .pipe(distinctUntilKeyChanged('name'))
+      .subscribe(value => console.log('distinctUntilKeyChanged:', value))
+
+  }
 }
