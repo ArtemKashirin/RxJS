cdaf73ba7108b48d975e61af407d89b00e10a57c
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 16:39:20 +0300

    Operators: sampleTime

--- src/app/functions/operators/sample-time/sample-time.component.ts
+++ src/app/functions/operators/sample-time/sample-time.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval} from "rxjs";
+import {sampleTime} from "rxjs/operators";
 
 @Component({
   selector: 'app-sample-time',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sample-time.component.scss']
 })
 export class SampleTimeComponent implements OnInit {
+  public sampleTimeLink: string = 'https://rxjs.dev/api/operators/sampleTime';
+  public sampleTime: string = 'sampleTime<T>(period: number, scheduler: SchedulerLike = asyncScheduler): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  sampleTim() {
+    interval(500)
+      .pipe(sampleTime(5000))
+      .subscribe(value => console.log('sampleTime:', value))
+
+  }
 }
