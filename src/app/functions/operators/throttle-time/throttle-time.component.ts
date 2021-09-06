9e8f12132ffa5e38041e402e79b2c6725969b207
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 16:49:51 +0300

    Operators: throttleTime

--- src/app/functions/operators/throttle-time/throttle-time.component.ts
+++ src/app/functions/operators/throttle-time/throttle-time.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval} from "rxjs";
+import {throttleTime} from "rxjs/operators";
 
 @Component({
   selector: 'app-throttle-time',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./throttle-time.component.scss']
 })
 export class ThrottleTimeComponent implements OnInit {
+  public throttleTimeLink: string = 'https://rxjs.dev/api/operators/throttleTime';
+  public throttleTime: string = 'throttleTime<T>(duration: number): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  throttleTim() {
+    interval(1000)
+      .pipe(throttleTime(5000))
+      .subscribe(value => console.log('throttleTime:', value))
+
+  }
 }
