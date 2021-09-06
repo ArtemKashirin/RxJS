84afdde964c6da23edccbe254cfb3533ef6ebaf0
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 16:32:47 +0300

    Operators: debounceTime

--- src/app/functions/operators/debounce-time/debounce-time.component.ts
+++ src/app/functions/operators/debounce-time/debounce-time.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {fromEvent, interval} from "rxjs";
+import {debounceTime} from "rxjs/operators";
 
 @Component({
   selector: 'app-debounce-time',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./debounce-time.component.scss']
 })
 export class DebounceTimeComponent implements OnInit {
+  public debounceTimeLink: string = 'https://rxjs.dev/api/operators/debounceTime';
+  public debounceTime: string = 'debounceTime<T>(dueTime: number, scheduler: SchedulerLike = asyncScheduler): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+
+  }
+
+  debounceTim() {
+    fromEvent(document, 'click')
+      .pipe(debounceTime(5000))
+      .subscribe(value => console.log('debounceTime:', value))
+
+  }
 }
