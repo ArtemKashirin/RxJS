6e3386771aa6f39437a2122563166422a8236689
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 16:44:39 +0300

    Operators: skipLast

--- src/app/functions/operators/skip-last/skip-last.component.ts
+++ src/app/functions/operators/skip-last/skip-last.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from "rxjs";
+import {skipLast} from "rxjs/operators";
 
 @Component({
   selector: 'app-skip-last',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skip-last.component.scss']
 })
 export class SkipLastComponent implements OnInit {
+  public skipLastLink: string = 'https://rxjs.dev/api/operators/skipLast';
+  public skipLast: string = 'skipLast<T>(skipCount: number): MonoTypeOperatorFunction<T>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  skipLas() {
+    of(1, 2, 3, 4, 5, 6, 7)
+      .pipe(skipLast(3))
+      .subscribe(value => console.log('skipLast:', value))
+  }
 }
