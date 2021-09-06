014e85c32a097a7a4e25b2376e619230302d201c
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 20:55:43 +0300

    Operators: defaultIfEmpty

--- src/app/functions/operators/default-if-empty/default-if-empty.component.ts
+++ src/app/functions/operators/default-if-empty/default-if-empty.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from "rxjs";
+import {defaultIfEmpty} from "rxjs/operators";
 
 @Component({
   selector: 'app-default-if-empty',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./default-if-empty.component.scss']
 })
 export class DefaultIfEmptyComponent implements OnInit {
+  public defaultIfEmptyLink: string = 'https://rxjs.dev/api/operators/defaultIfEmpty';
+  public defaultIfEmpty: string = 'defaultIfEmpty<T, R>(defaultValue: R): OperatorFunction<T, T | R>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  defaultIfEmpt() {
+    of<any>()
+      .pipe(defaultIfEmpty("0"))
+      .subscribe(value => console.log('defaultIfEmpty:', value))
+
+  }
 }
