fc8537f8a22309219561254861e4d66f9226241c
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 21:03:39 +0300

    Operators: startWith

--- src/app/functions/operators/start-with/start-with.component.ts
+++ src/app/functions/operators/start-with/start-with.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from "rxjs";
+import {startWith} from "rxjs/operators";
 
 @Component({
   selector: 'app-start-with',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-with.component.scss']
 })
 export class StartWithComponent implements OnInit {
+  public startWithLink: string = 'https://rxjs.dev/api/operators/startWith';
+  public startWith: string = 'startWith<T, D>(...values: D[]): OperatorFunction<T, T | D>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  startWit() {
+    of(1, 2, 3, 4, '5')
+      .pipe(startWith(10))
+      .subscribe(value => console.log('startWith:', value))
+  }
 }
