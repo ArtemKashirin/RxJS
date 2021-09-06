75aa846a8fe1f8106bd4470c90d643f9c3e0023e
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 15:44:15 +0300

    Operators: windowTime

--- src/app/functions/operators/window-time/window-time.component.ts
+++ src/app/functions/operators/window-time/window-time.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval} from "rxjs";
+import {windowTime} from "rxjs/operators";
 
 @Component({
   selector: 'app-window-time',
@@ -6,12 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./window-time.component.scss']
 })
 export class WindowTimeComponent implements OnInit {
-  // public Link: string = '';
-  // public Link: string = '';
+  public windowTimeLink: string = 'https://rxjs.dev/api/operators/windowTime';
+  public windowTime: string = 'windowTime<T>(windowTimeSpan: number, ...otherArgs: any[]): OperatorFunction<T, Observable<T>>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  windowTim() {
+    interval(1000)
+      .pipe(windowTime(5000))
+      .subscribe(value => console.log(value.subscribe(value1 => console.log('windowTime:', value1))))
+  }
 }
