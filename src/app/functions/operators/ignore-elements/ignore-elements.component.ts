0add6b195444231f0dd49df0b8e7cc91e4b0b230
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 17:04:07 +0300

    Operators: ignoreElements

--- src/app/functions/operators/ignore-elements/ignore-elements.component.ts
+++ src/app/functions/operators/ignore-elements/ignore-elements.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval} from "rxjs";
+import {ignoreElements, take} from "rxjs/operators";
 
 @Component({
   selector: 'app-ignore-elements',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ignore-elements.component.scss']
 })
 export class IgnoreElementsComponent implements OnInit {
+  public ignoreElementsLink: string = 'https://rxjs.dev/api/operators/ignoreElements';
+  public ignoreElements: string = 'ignoreElements(): OperatorFunction<any, never>';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  ignoreElement() {
+    interval(500).pipe(
+      take(4),
+      ignoreElements())
+      .subscribe(undefined, undefined, () => console.log('ignoreElements:', 'complete'))
+
+  }
 }
