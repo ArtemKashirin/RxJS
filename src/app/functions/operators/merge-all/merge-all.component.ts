da7ee9d959207ceded388367ead1ea9d925faa40
artem_kashirin <artem.kashirin1986@gmail.com>
2021-09-06 20:00:42 +0300

    Operators: mergeAll

--- src/app/functions/operators/merge-all/merge-all.component.ts
+++ src/app/functions/operators/merge-all/merge-all.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {interval, of} from "rxjs";
+import {map, mergeAll, take} from "rxjs/operators";
 
 @Component({
   selector: 'app-merge-all',
@@ -6,12 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./merge-all.component.scss']
 })
 export class MergeAllComponent implements OnInit {
-  // public Link: string = '';
-  // public Link: string = '';
+  public mergeAllLink: string = 'https://rxjs.dev/api/operators/mergeAll';
+  public mergeAll: string = 'mergeAll<O extends ObservableInput<any>>(concurrent: number = Infinity): OperatorFunction<O, ObservedValueOf<O>>';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  mergeAl() {
+    of(1, 2, 3, 4)
+      .pipe(
+        map(ev => interval(100).pipe(take(4))))
+      .pipe(mergeAll())
+      .subscribe(value => console.log('mergeAll:', value));
+  }
 }
